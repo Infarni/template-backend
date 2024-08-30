bb5562d6672d396bdb6028de0e3a4e6f99f4ff8f
archdrdr <archdroider@proton.me>
2024-08-30 14:16:44 +0300

    Create base module and model

---
+++ rps_backend/auth/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from rps_backend.database.models import BaseModel
+
+
+class UserModel(BaseModel):
+    __tablename__ = "user"
+
+    username: Mapped[str]
+    email: Mapped[str]
+    hashed_password: Mapped[str]
+    avatar: Mapped[bytes | None]
